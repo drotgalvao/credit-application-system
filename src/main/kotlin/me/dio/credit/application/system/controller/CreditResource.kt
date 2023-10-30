db48288bab0c8f18232f92961b7b6518a258617d
Diego <diegohenriquegalvao@gmail.com>
2023-10-30 16:55:16 -0300

    ResponseEntity fix

--- src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -1,41 +1,52 @@
 package me.dio.credit.application.system.controller
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.PathVariable
-import me.dio.credit.application.system.service.impl.CreditService
-import me.dio.credit.application.system.dto.CreditDto
-import me.dio.credit.application.system.dto.CreditViewList
-import me.dio.credit.application.system.dto.CreditView
-import me.dio.credit.application.system.entity.Credit
-import java.util.stream.Collectors
 import java.util.UUID
+import java.util.stream.Collectors
+import me.dio.credit.application.system.dto.CreditDto
+import me.dio.credit.application.system.dto.CreditView
+import me.dio.credit.application.system.dto.CreditViewList
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.service.impl.CreditService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/credits")
-class CreditResource (private val creditService: CreditService) {
+class CreditResource(private val creditService: CreditService) {
 
     @PostMapping
-    fun saveCredit(@RequestBody creditDto: CreditDto): String {
-        val credit : Credit = this.creditService.save(creditDto.toEntity())
-        return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    fun saveCredit(@RequestBody creditDto: CreditDto): ResponseEntity<String> {
+        val credit: Credit = this.creditService.save(creditDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!")
     }
 
     @GetMapping
-    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
-        return this.creditService.findAllByCustomer(customerId).stream().map { credit : Credit -> CreditViewList(credit) }.collect(Collectors.toList())
+    fun findAllByCustomerId(
+            @RequestParam(value = "customerId") customerId: Long
+    ): ResponseEntity<List<CreditViewList>> {
+        val creditViewList: List<CreditViewList> =
+                this.creditService
+                        .findAllByCustomer(customerId)
+                        .stream()
+                        .map { credit: Credit -> CreditViewList(credit) }
+                        .collect(Collectors.toList())
+        return ResponseEntity.status(HttpStatus.OK).body(creditViewList)
     }
 
     @GetMapping("/{creditCode}")
-    fun findByCreditCode(@PathVariable creditCode: UUID, @RequestParam(value = "customerId") customerId: Long): CreditView {
+    fun findByCreditCode(
+            @PathVariable creditCode: UUID,
+            @RequestParam(value = "customerId") customerId: Long
+    ): ResponseEntity<CreditView> {
         val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
-        return CreditView(credit)
+        return ResponseEntity.status(HttpStatus.OK).body(CreditView(credit))
     }
-
-    
-
 }
